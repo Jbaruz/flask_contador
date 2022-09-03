644b5cbaf9432fba8ce2e34de081bff985236092
Jbaruz <jbaruz@gmail.com>
2022-09-03 17:04:43 -0500

    Se agrego bonus

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 - Crea un nuevo proyecto Flask llamado contador (HECHO)
 - Haz que la ruta raíz renderice una plantilla que muestre la cantidad de veces que el cliente ha visitado este sitio. Actualiza la página varias veces para asegurarte de que el contador esté funcionando. (HECHO)
 - Agrega una ruta "/ destroy_ session" que elimine la sesión y redirija a la ruta raíz. Pruébalo. (HECHO)
+- BONUS NINJA: agrega un botón +2 debajo del contador y una nueva ruta que incremente el contador en 2 (HECHO)
+- BONUS NINJA: agrega un botón de reinicio para reiniciar el contador (HECHO)
+- BONUS SENSEI: agrega un formulario que le permita al usuario especificar el increment del contador y haz que el contador se incremente en consecuencia (HECHO).
--- server.py
+++ server.py
@@ -1,16 +1,16 @@
-from ast import Num
 from crypt import methods
 from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
 app.secret_key ='cualquier cosa que sea secreta' #Codigo de seguridad, se usa con session/si se cambia y reinicia el servidor los usuarios desloguean
 
 
-
-@app.route('/') 
+@app.route('/', methods=['GET'])
 def count():
-    if 'counter' in session:
+    if 'counter' in session and 'visits' in session:
+        session['visits']+=1
         session['counter']+=1 # Para sumar de +1
     else:
+        session['visits']=1
         session['counter'] = 1 # Para regresa a 1
     return render_template('index.html')
 
@@ -18,6 +18,17 @@ def count():
 def destroy():
     session.clear()
     return redirect('/') #Permite regresar al index con el conteo a 1 (eliminado la sesion)
+
+@app.route('/count_by_two', methods=['GET'])
+def count_x_two():
+    session['counter']+=2 # Para sumar de +2
+    return render_template('/index.html')
+
+@app.route('/add_by', methods=['POST'])
+def count_by_num():
+    num=request.form['counter'] #Agrego counter porque necesito info de la session inicial('/')
+    session['counter']+=int(num)
+    return render_template('/index.html')
 
 if __name__=="__main__":
     app.run(debug=True)
